6445a7267f7454a9f9ccbb22b625055a784d5c0d
Shubhangi <shubhangi@softobiz.com>
2023-07-25 16:33:30 +0530

    graphql setup and use code first approach

---
+++ src/book/book.schema.ts
@@ -0,0 +1,16 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+
+@ObjectType()
+export class Book {
+    
+  @Field((type) => Int)
+  id: number;
+
+  @Field()
+  title?: string;
+
+  @Field((type) => Int, {nullable:true})    
+  price?: number;
+
+}   
